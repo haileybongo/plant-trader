e0c222eb39ea39d1582544543a26e7e73df78701
haileybongo <haileyebongo@gmail.com>
2020-12-28 15:18:41 -0800

    updating nav bar

---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+  
+import React from 'react'
+
+
+const Home = () => {
+
+  return (
+    <div style={{fontSize: '40px',   float:'left', fontWeight: '900'}} className="container" width="100px" >
+        <br/>
+        Test Home
+    </div>
+
+  )
+}
+
+export default Home
